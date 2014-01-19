3471acc4caadd36a8fc8933e9b94dbb80cf88de6
MainMemory <sonicmike2@yahoo.com>
2014-01-19 11:07:53 -0600

    Initial Commit

---
+++ IniFile/IniFile/IniFile.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IniDictionary = System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, string>>;
+using IniGroup = System.Collections.Generic.Dictionary<string, string>;
+using IniNameGroup = System.Collections.Generic.KeyValuePair<string, System.Collections.Generic.Dictionary<string, string>>;
+using IniNameValue = System.Collections.Generic.KeyValuePair<string, string>;
+
+namespace IniFile
+{
+    public static class IniFile
+    {
+        public static IniDictionary Load(string filename)
+        {
+            IniDictionary result = new IniDictionary();
+            IniGroup curent = new IniGroup();
+            result.Add(string.Empty, curent);
+            string curgroup = string.Empty;
+            string[] fc = System.IO.File.ReadAllLines(filename);
+            for (int i = 0; i < fc.Length; i++)
+            {
+                string line = fc[i];
+                StringBuilder sb = new StringBuilder(line.Length);
+                bool startswithbracket = false;
+                int firstequals = -1;
+                int endbracket = -1;
+                for (int c = 0; c < line.Length; c++)
+                    switch (line[c])
+                    {
+                        case '\\': // escape character
+                            if (c + 1 == line.Length) goto default;
+                            c++;
+                            switch (line[c])
+                            {
+                                case 'n': // line feed
+                                    sb.Append('\n');
+                                    break;
+                                case 'r': // carriage return
+                                    sb.Append('\r');
+                                    break;
+                                default: // literal character
+                                    sb.Append(line[c]);
+                                    break;
+                            }
+                            break;
+                        case '=':
+                            if (firstequals == -1)
+                                firstequals = sb.Length;
+                            goto default;
+                        case '[':
+                            if (c == 0)
+                                startswithbracket = true;
+                            goto default;
+                        case ']':
+                            endbracket = sb.Length;
+                            goto default;
+                        case ';': // comment character, stop processing this line
+                            c = line.Length;
+                            break;
+                        default:
+                            sb.Append(line[c]);
+                            break;
+                    }
+                line = sb.ToString();
+                if (startswithbracket & endbracket != -1)
+                {
+                    curgroup = line.Substring(1, endbracket - 1);
+                    curent = new IniGroup();
+                    try
+                    {
+                        result.Add(curgroup, curent);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        throw new Exception("INI File error: Group \"" + curgroup + "\" already exists.\n" + filename + ":line " + i, ex);
+                    }
+                }
+                else if (!IsNullOrWhiteSpace(line))
+                {
+                    string key;
+                    string value = string.Empty;
+                    if (firstequals > -1)
+                    {
+                        key = line.Substring(0, firstequals);
+                        value = line.Substring(firstequals + 1);
+                    }
+                    else
+                        key = line;
+                    try
+                    {
+                        curent.Add(key, value);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        throw new Exception("INI File error: Value \"" + key + "\" already exists in group \"" + curgroup + "\".\n" + filename + ":line " + i, ex);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static void Save(IniDictionary INI, string filename)
+        {
+            List<string> result = new List<string>();
+            foreach (IniNameGroup group in INI)
+            {
+                if (!string.IsNullOrEmpty(group.Key))
+                    result.Add("[" + group.Key.Replace(@"\", @"\\").Replace("\n", @"\n").Replace("\r", @"\r").Replace(";", @"\;") + "]");
+                foreach (IniNameValue value in group.Value)
+                {
+                    string escapedkey = value.Key.Replace(@"\", @"\\").Replace("=", @"\=").Replace("\n", @"\n").Replace("\r", @"\r").Replace(";", @"\;");
+                    if (escapedkey.StartsWith("["))
+                        escapedkey = escapedkey.Insert(0, @"\");
+                    result.Add(escapedkey + "=" + value.Value.Replace(@"\", @"\\").Replace("\n", @"\n").Replace("\r", @"\r").Replace(";", @"\;"));
+                }
+            }
+            System.IO.File.WriteAllLines(filename, result.ToArray());
+        }
+
+        public static IniDictionary Combine(IniDictionary dictA, IniDictionary dictB)
+        {
+            IniDictionary result = new IniDictionary();
+            foreach (IniNameGroup group in dictA)
+                result.Add(group.Key, new IniGroup(group.Value));
+            foreach (IniNameGroup group in dictB)
+                if (result.ContainsKey(group.Key))
+                    foreach (IniNameValue item in group.Value)
+                        result[group.Key][item.Key] = item.Value;
+                else
+                    result.Add(group.Key, new IniGroup(group.Value));
+            return result;
+        }
+
+        internal static bool IsNullOrWhiteSpace(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return true;
+            for (int i = 0; i < value.Length; i++)
+                if (!char.IsWhiteSpace(value[i]))
+                    return false;
+            return true;
+        }
+    }
+}
